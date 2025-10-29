1d964f4c0ccd93966b96105fb2f11465ccead724
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-10-29 12:57:52 -0300

    feat: map custom fields for bills

--- target_intacct_v3/mappers/bill_schema_mapper.py
+++ target_intacct_v3/mappers/bill_schema_mapper.py
@@ -27,7 +27,8 @@ class BillSchemaMapper(BaseMapper):
             **self._map_internal_id(),
             **self._map_subsidiary(),
             **self._map_is_draft(),
-            **self._map_sub_record("Vendors", "VENDORID", record_no_field="vendorId", record_id_field="vendorNumber", record_name_field="vendorName", subsidiary_id=self.subsidiary_id)
+            **self._map_sub_record("Vendors", "VENDORID", record_no_field="vendorId", record_id_field="vendorNumber", record_name_field="vendorName", subsidiary_id=self.subsidiary_id),
+            **self._map_custom_fields()
         }
      
         self._map_line_items_and_expenses(payload)
